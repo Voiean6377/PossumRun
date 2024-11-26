0e37647baab4226e6ade22c9b73a5b19678dbd20
Voiean6377 <153617342+Voiean6377@users.noreply.github.com>
2024-11-26 21:10:23 +0800

    Added Sprite, Tilemap, Tiles and Controls for Sprite

--- main.ts
+++ main.ts
@@ -1,1 +1,28 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -150
+})
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`black`)
+tiles.setCurrentTilemap(tilemap`level0`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c c c . 
+    . . . . . . . . . c c c c c c c 
+    . . . . . . . . . c c c c c c c 
+    . . . . e e e e e e c c c c c c 
+    . . . e e e e e e e e c c c c . 
+    . . . e e e e e e e e e . . . . 
+    . . . e e e e e e e e e . . . . 
+    . . . . e e e e e e e . . . . . 
+    . . . . . e . . . . e . . . . . 
+    . . . . . e . . . . e . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(31, 220)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay = 500
+scene.cameraFollowSprite(mySprite)
