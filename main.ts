a7dd39d45ea34a7ceddea5057228a118eb4f5f64
Voiean6377 <153617342+Voiean6377@users.noreply.github.com>
2024-11-28 18:35:29 +0800

    Changed Location of the Hole, Added "Memory Fragments" (Points) and Landmines. (Enemy) Fixed Bug where pressing jump multiple times let you "fly".

--- main.ts
+++ main.ts
@@ -1,9 +1,30 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.vy = -150
+scene.onOverlapTile(SpriteKind.Player, assets.tile`memory_2`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (game.runtime() - LastPressed >= TimebetweenPresses) {
+        mySprite.vy = -150
+        LastPressed = game.runtime()
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`mine`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeLifeBy(-1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`memory_3`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`memory_1`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
+})
+let TimebetweenPresses = 0
+let LastPressed = 0
 let mySprite: Sprite = null
 scene.setBackgroundImage(assets.image`black`)
-tiles.setCurrentTilemap(tilemap`level0`)
+tiles.setCurrentTilemap(tilemap`level_1`)
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -22,7 +43,9 @@ mySprite = sprites.create(img`
     . . . . . e . . . . e . . . . . 
     . . . . . e . . . . e . . . . . 
     `, SpriteKind.Player)
-mySprite.setPosition(31, 220)
+mySprite.setPosition(32, 185)
 controller.moveSprite(mySprite, 100, 0)
 mySprite.ay = 500
 scene.cameraFollowSprite(mySprite)
+LastPressed = 0
+TimebetweenPresses = 600
